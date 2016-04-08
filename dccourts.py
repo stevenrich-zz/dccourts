f806b80f08cae68f12d38ee0309fd068af7b5760
Steven Rich <steven.rich@washpost.com>
2016-04-07 21:41:07 -0400

    Update dccourts.py

--- dccourts.py
+++ dccourts.py
@@ -1,1 +1,12 @@
+import time
+import re
+from BeautifulSoup import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from pyvirtualdisplay import Display
+import csv
 
+print "Scraping DC Superior Court site..."
+
+display = Display(visible=0, size=(800,600))
+display.start()
