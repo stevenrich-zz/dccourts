f1fa87850f016f28a73d6500e3f3fc443371cbe2
Steven Rich <steven.rich@washpost.com>
2016-04-08 19:56:03 -0400

    Update dccourts.py

--- dccourts.py
+++ dccourts.py
@@ -10,3 +10,7 @@ print "Scraping DC Superior Court site..."
 
 display = Display(visible=0, size=(800,600))
 display.start()
+
+alph1 = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
+alph2 = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
+alph3 = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
