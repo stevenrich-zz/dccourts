5e0d1b4dc2027c181ec5a3c330d55bc2dfb0e094
Steven Rich <steven.rich@washpost.com>
2016-04-08 20:00:45 -0400

    Update dccourts.py

--- dccourts.py
+++ dccourts.py
@@ -14,3 +14,15 @@ display.start()
 alph1 = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
 alph2 = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
 alph3 = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
+
+for a in alph1:
+  for b in alph2:
+    for c in alph3:
+      driver = webdriver.Firefox()
+  		driver.get("https://www.dccourts.gov/cco/maincase.jsf")
+  		element = driver.find_element_by_name("appData:searchform:jspsearchpage:lastName")
+  		element.send_keys(str(a) + str(b))
+  		element2 = driver.find_element_by_name("appData:searchform:jspsearchpage:firstName")
+  		element2.send_keys(str(c))
+  		driver.find_element_by_name("appData:searchform:jspsearchpage:submitSearch").click()
+  		page = driver.page_source
