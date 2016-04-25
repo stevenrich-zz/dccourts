b8037fad9290f12f9daa02fc794d24c7a3079285
Steven Rich <steven.rich@washpost.com>
2016-04-24 23:48:41 -0400

    Update dccourts.py

--- dccourts.py
+++ dccourts.py
@@ -12,7 +12,7 @@ print "Scraping DC Superior Court site..."
 display = Display(visible=0, size=(800,600))
 display.start()
 
-compile_file = open("compile_file.txt", "w")
+compile_file = open("compile_file.txt", "a")
 
 alph1 = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 alph2 = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
